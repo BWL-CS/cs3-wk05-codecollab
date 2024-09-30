51f2bdc86d0a6be4e0f54121789985acb4526f9e
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-30 11:06:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
-def main():
-    print("hello world")
+from numpy import divide
+import pandas as pd
 
+# PART A
+df = pd.DataFrame({
+    "Name": [
+        "Braund, Mr. Owen Harris",
+        "Allen, Mr. William Henry",
+        "Bonnell, Miss. Elizabeth",
+    ],
+    "Age": [22, 35, 58],
+    "Sex": ["male", "male", "female"],
+})
 
-if __name__ == "__main__":
-    main()
+# Divider to help organize terminal output
+DIV = "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️"
+
+print(df)
+print(DIV)
+
+print(df.describe())
+print(DIV)
+
+print(df["Age"])
+print(DIV)
+
+ages = pd.Series([22, 35, 58], name="Age")
+print(ages)
+print(DIV)
+
+# PART B
+titanic = pd.read_csv("titanic.csv")
+
+print(titanic)
+print(DIV)
+
+print(titanic.info())
+print(DIV)
+
+# PART C
+above_35 = titanic[titanic["Age"] > 35]
+
+print(above_35.shape)
+print(DIV)
+
+adult_names = titanic.loc[titanic["Age"] > 35, "Name"]
+
+print(adult_names.head())
+print(DIV)
+
+# PART D
+print(titanic["Name"].str.lower())
+print(DIV)
+
+print(titanic["Name"].str.split(","))
+print(DIV)
+
+print(titanic[titanic["Name"].str.contains("Countess")])
