c6e8a31b5031ed339a03a1bf744b5a0c9942a393
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-10-01 13:51:12 +0000

    reorganized

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from numpy import divide
 import pandas as pd
 
 # WARNING: Lots of print statements here!!! Sorry!
@@ -7,22 +6,44 @@ import pandas as pd
 DIV = "〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️"
 
 # PART A: Series & DataFrames
-dict = {'Braund, Mr. Owen Harris': 22,
-        'Allen, Mr. William Henry': 35,
-        "Bonnell, Miss. Elizabeth": 58}
+print("***** PART A *****")
+
+dict = {'Kamala': 59,
+        'Tim': 60,
+        'Donald': 78,
+        'JustDance Vance':40}
 series = pd.Series(dict)
-dataframe = pd.DataFrame(series, columns=['age'])
+df1 = pd.DataFrame(series, columns=['age'])
 
-print(dataframe) # how many dimensions is this DF?
+print(df1)
 print(DIV)
 
-print(dataframe.describe()) 
+print(df1.describe()) 
 print(DIV)
 
-# PART B: DataFrame from a CSV file
+# PART B: Compare df2 to df1?
+print("***** PART B *****")
+
+area = pd.Series({'California': 423967, 'Texas': 695662,
+                  'Florida': 170312, 'New York': 141297,
+                  'Pennsylvania': 119280})
+pop = pd.Series({'California': 39538223, 'Texas': 29145505,
+                 'Florida': 21538187, 'New York': 20201249,
+                 'Pennsylvania': 13002700})
+df2 = pd.DataFrame({'area':area, 'pop':pop})
+
+print(df2) 
+print(DIV)
+
+print(df2.describe()) 
+print(DIV)
+
+# PART C: DataFrame from CSV file
+print("***** PART C *****")
+
 titanic = pd.read_csv("titanic.csv")
 
-print(titanic) # how does this DF differ from Part A?
+print(titanic)
 print(DIV)
 
 print(titanic.info())
@@ -31,18 +52,9 @@ print(DIV)
 print(titanic.columns) 
 print(DIV)
 
-# PART C: Filtering
-above_35 = titanic[titanic['Age'] > 35]
-
-print(above_35.shape)
-print(DIV)
-
-adult_names = titanic.loc[titanic['Age'] > 35, 'Name']
-
-print(adult_names.head())
-print(DIV)
-
 # PART D: Manipulating Text Content
+print("***** PART D *****")
+
 print(titanic['Name'].str.lower())
 print(DIV)
 
